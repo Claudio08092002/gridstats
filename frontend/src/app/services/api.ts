0c7b7f2630f31e105c9fa0d51849547b25e01e43
Claudio <claudio@giacal.ch>
2025-09-21 23:57:26 +0200

    changed api stuff

--- frontend/src/app/services/api.ts
+++ frontend/src/app/services/api.ts
@@ -1,7 +1,7 @@
-// frontend/src/app/services/api.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../enviroments/enviroments';
 
 export interface DriverSummary {
   name: string;
@@ -23,7 +23,7 @@ export interface SeasonResponse {
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  loadSeason(year: number) {
-    return this.http.get<SeasonResponse>(`/api/f1/season/${year}`);
+  loadSeason(year: number): Observable<SeasonResponse> {
+    return this.http.get<SeasonResponse>(`${environment.apiBase}/f1/season/${year}`);
   }
 }
--- frontend/src/enviroments/enviroment.prod.ts
+++ frontend/src/enviroments/enviroment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBase: 'https://gridstats-api.stefanhohl.ch/'
+  apiBase: 'https://gridstats-api.stefanhohl.ch/api'
 };
